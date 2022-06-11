f83e653ae2c34a0e69c710468416155bd2eb39cd
Packiaseelan S <packiaseelan14@gmail.com>
2022-06-11 21:26:51 +0530

    Example app added

---
+++ HexLocalisationExample/HexLocalisationExample/HexLocalisationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexLocalisationExampleApp.swift
+//  HexLocalisationExample
+//
+//  Created by Packiaseelan S on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct HexLocalisationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
